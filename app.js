4e357de081a96c9e7db21d3c6bbaabafaa0b2bff
T.J <t.ruggles@live.ca>
2018-04-30 22:07:55 -0400

    Added action log for each turn

--- app.js
+++ app.js
@@ -3,23 +3,35 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
   methods: {
     startGame: function() {
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function() {
-      this.monsterHealth -= this.calculateDamage(3, 10);
+      const damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits monster for ' + damage + '.'
+      });
       if (this.checkWin()) {
         return;
       }
       this.monsterAttacks();
     },
     specialAttack: function() {
-      this.monsterHealth -= this.calculateDamage(10, 20);
+      const damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits monster hard for ' + damage + '.'
+      });
       if (this.checkWin()) {
         return;
       }
@@ -35,6 +47,10 @@ new Vue({
       } else {
         this.playerHealth += 10;
       }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals for 10.'
+      });
       this.monsterAttacks();
     },
     giveUp: function() {
@@ -64,8 +80,13 @@ new Vue({
       }
     },
     monsterAttacks: function() {
-      this.playerHealth -= this.calculateDamage(5, 12);
+      const damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage; 
       this.checkWin();
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits player for ' + damage + '.'
+      });
     }
   }
 });
